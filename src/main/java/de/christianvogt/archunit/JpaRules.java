618deb92782a62dee7b734297e91fa65ed59fabb
christianvogt <mail@christianvogt.de>
2019-11-24 19:27:44 +0100

    Getting ready for Maven Central release

--- src/main/java/de/christianvogt/archunit/JpaRules.java
+++ src/main/java/de/christianvogt/archunit/JpaRules.java
@@ -5,8 +5,13 @@ import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 
 import javax.persistence.Entity;
 
+/**
+ * JPA-ArchRules to be used in ArchUnit-Tests. Run with @ArchTest
+ *
+ */
 public class JpaRules {
     private static String description = "not contain fields or methods that are eagerly fetched";
+    /** Find eagerly fetched JPA associations in JPA Entities. */
     public static ArchRule NO_EAGER_LOADING_ENTITY_ASSOCIATIONS = ArchRuleDefinition
             .classes().that()
             .areAnnotatedWith(Entity.class)
